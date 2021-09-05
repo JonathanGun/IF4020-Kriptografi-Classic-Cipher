ef3de99758730379864804cdcb82855a07a9ed62
jonathangun <jonathanyudigun@gmail.com>
2021-09-05 23:35:48 +0700

    affine cipher

--- ciphers/affine.py
+++ ciphers/affine.py
@@ -2,10 +2,36 @@ from ciphers.base import Cipher
 
 
 class AffineCipher(Cipher):
+    def egcd(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = AffineCipher.egcd(b % a, a)
+            return (g, x - (b // a) * y, y)
+
+    def modinv(a, m):
+        g, x, y = AffineCipher.egcd(a, m)
+        if g != 1:
+            raise Exception('modular inverse does not exist')
+        else:
+            return x % m
+
     def encrypt(msg: str, key: str) -> str:
-        # TODO
-        return msg + "ASD"
+        m, b = map(int, key.split(","))
+        ret = []
+        for p in msg:
+            c = (m * (ord(p) - ord('A')) + b) % 26
+            c = chr(c + ord('A'))
+            ret.append(c)
+        return "".join(ret)
 
     def decrypt(msg: str, key: str) -> str:
-        # TODO
-        return "ASD" + msg
+        m, b = map(int, key.split(","))
+        mod_m = AffineCipher.modinv(m, 26)
+
+        ret = []
+        for c in msg:
+            p = (mod_m * (ord(c) - ord('A') - b)) % 26
+            p = chr(p + ord('A'))
+            ret.append(p)
+        return "".join(ret)
