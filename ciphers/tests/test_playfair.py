61349559fc3a7bc285c20af3d45a2f908c29103e
jonathangun <jonathanyudigun@gmail.com>
2021-09-05 23:15:37 +0700

    fix playfair test

--- ciphers/tests/test_playfair.py
+++ ciphers/tests/test_playfair.py
@@ -2,8 +2,8 @@ from ciphers.playfair import PlayfairCipher
 
 
 def test_simple():
-    plaintext = "temui ibu nanti malam"
-    key = "jalan ganesha sepuluh"
+    plaintext = "TEMUIIBUNANTIMALAM"
+    key = "JALAN GANESHA SEPULUH"
     ciphertext = PlayfairCipher.encrypt(plaintext, key)
     expected = "ZBRSFYKUPGLGRKVSNLQV"
     assert ciphertext == expected
